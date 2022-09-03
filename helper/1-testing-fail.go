c4bb033b7b26a8b7e41b896a04952d0a4981a790
rasyidev <habib.rasyid11@gmail.com>
2022-09-03 12:32:57 +0700

    Ubah pesan error biar standar

--- helper/1-testing-fail.go
+++ helper/1-testing-fail.go
@@ -4,8 +4,12 @@ import "errors"
 
 func Pembagian(a, b float64) (float64, error) {
 	if b == 0 {
-		return 0, errors.New("Tidak bisa membagi dengan angka nol")
+		return 0, errors.New("tidak bisa membagi dengan angka nol")
 	}
 
 	return a / b, nil
+}
+
+func HelloRasyidev(name string) string {
+	return "Halo " + name
 }
