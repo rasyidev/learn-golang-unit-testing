9c683669232792bde113402cf897f6dd24b12f32
rasyidev <habib.rasyid11@gmail.com>
2022-09-04 14:05:13 +0700

    Add testing output example

--- helper/4-testing-skip_test.go
+++ helper/4-testing-skip_test.go
@@ -12,3 +12,13 @@ func TestCheckOS(t *testing.T) {
 	}
 	fmt.Println("Selesai menjalankan test CheckOS")
 }
+
+/*
+$ go test -v -run TestCheckOS
+=== RUN   TestCheckOS
+windows
+    4-testing-skip_test.go:11: Skip test untuk operasi sistem Windows
+--- SKIP: TestCheckOS (0.00s)
+PASS
+ok      learn-go-unit-test/helper       0.821s
+*/
